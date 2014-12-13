eb5c2024f59b54d91b4a9f3a954c6d54b43367ae
Sebastian Wenzel <sebastian.wenzel@honestly.de>
2014-12-13 16:10:07 +0100

    time out after change

--- keyword2image.js
+++ keyword2image.js
@@ -1,31 +1,32 @@
 
 function getImageUrl(keyword,callback){
 	var apiKey = "eb256520904bd8ee05b1116f2ed5da99";
-	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=' + apiKey + '&text=' + keyword,
+	var param  = "tags";
+	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=' + apiKey + '&'+param+'=' + keyword,
     	callback);
 }
 function photoObject2ImageUrl(photo){
 	return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
 }
 function imagecallback(data){
-	console.debug(data.photos);
 	if(data.photos && data.photos.photo && data.photos.photo.length>0){
 		console.debug(data.photos.photo[0]);
 		console.debug(photoObject2ImageUrl(data.photos.photo[0]));
 		$('#canvas').css("background-image", "url("+photoObject2ImageUrl(data.photos.photo[0])+")");
 	}
 }
+function refreshPic(){
+	var input = $("#text_input").val();
+	var splitted = input.split(" ");
+	var lastWord = splitted[splitted.length-1];
+	getImageUrl(lastWord,imagecallback);
+}
 $( document ).ready(function() {
-	var lasttime = 0;
+	var lastchangetime = 0;
+	var timer = null;
 	$('#text_input').on('input', function(){
-		var current = new Date();
-		var diff = (current - lasttime)/ 1000; // in second
-		if(diff > 1){
-			lasttime = new Date();
-			var input = $("#text_input").val();
-			var splitted = input.split(" ");
-			var lastWord = splitted[splitted.length-1];
-			getImageUrl(input,imagecallback);
-		}
+		clearTimeout(timer);
+		timer = setTimeout(refreshPic, 400);
 	});
+
 });
