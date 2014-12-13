f4d2d780084c1609dbb34775ceb04ad697a0da98
Julius Pfrommer <julius.pfrommer@kit.edu>
2014-12-13 15:47:40 +0100

    local changes

--- presentation.js
+++ presentation.js
@@ -1,5 +1,17 @@
-$( document ).ready(function() {
+function setText() {
+    if(!($("#title").is(":focus"))) {
+        $('#hidden_input').select();
+        var text = $('#hidden_input').val();
+        if(text == '') {
+            $('#title').val("Sprich Freund und tritt ein");
+        } else {
+            $('#title').val(text);
+        }
+    }
+}
 
-    console.log( "ready!" );
+$( document ).ready(function() {
     $('#canvas').css("background-image", "url(http://spinoff.comicbookresources.com/wp-content/uploads/2014/06/mr-t.jpeg)");  
+    setText();
+    setInterval(setText, 2000);
 });
