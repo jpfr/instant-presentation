00e8fae3a56f78a274dde93316a2c014f8bc71dd
Sebastian Wenzel <sebastian.wenzel@honestly.de>
2014-12-13 16:37:26 +0100

    hd pics only photos

--- instant-presentation.js
+++ instant-presentation.js
@@ -1,10 +1,10 @@
 function getImageUrl(keyword,callback){
 	var apiKey = "eb256520904bd8ee05b1116f2ed5da99";
-	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&sort=relevance&api_key=' + apiKey + '&text=' + keyword,
+	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&content_type=1&sort=relevance&api_key=' + apiKey + '&text=' + keyword,
     	callback);
 }
 function photoObject2ImageUrl(photo){
-	return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
+	return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_b.jpg";
 }
 
 function imagecallback(data){
@@ -19,7 +19,9 @@ function refreshPic(){
 	var input = $("#title").val();
 	var splitted = input.split(" ");
 	var lastWord = splitted[splitted.length-1];
-	getImageUrl(lastWord,imagecallback);
+	if(lastWord.length > 4){
+		getImageUrl(lastWord,imagecallback);
+	}
 }
 $( document ).ready(function() {
     $('#canvas').css("background-image", "url(http://spinoff.comicbookresources.com/wp-content/uploads/2014/06/mr-t.jpeg)");  
