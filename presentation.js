a897a0e750c31cf9e64a047ae36a7064df7f54c2
Julius Pfrommer <julius.pfrommer@kit.edu>
2014-12-13 15:17:06 +0100

    Mr T!!

---
+++ presentation.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+
+    console.log( "ready!" );
+    $('#canvas').css("background-image", "url(http://spinoff.comicbookresources.com/wp-content/uploads/2014/06/mr-t.jpeg)");  
+});
